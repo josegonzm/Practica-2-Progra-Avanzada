33c80ed7f49b6268150037fbe8c66776f3a9563a
josegonzm <133930592+josegonzm@users.noreply.github.com>
2023-10-09 19:15:44 -0600

    Creacion de proyecto

---
+++ Practica-2-PrograAvanzada/Practica-2-PrograAvanzada/Menu.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2_PrograAvanzada
+{
+    internal class Menu
+    {
+        public void MostrarMenu()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Menu Hoteles \n" +
+                    "1- Listar Hoteles  \n" +
+                    "2- Listar habitaciones disponibles por hotel ?\n" +
+                    "3- Registrar reservación a cliente automáticamente \n" +
+                    "4- Registrar reservación a cliente manualmente \n" +
+                    "5- Eliminar reservación ?\n" +
+                    "6- Eliminar todas las reservaciones por hotel ?\n" +
+                    "7- Buscar persona por número de cédula o pasaporte \n" +
+                    "8- Validar disponibilidad de habitación en hotel \n" +
+                    "9- Salir");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        subMenuOpcion2();
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "4":
+                        
+                        String nombreHotel = Console.ReadLine();
+                        switch (nombreHotel)
+                        {
+                            case "Hotel New York":
+                                
+                                break;
+
+                            case "Hotel Continental de Roma":
+
+                                break;
+                            case "Hotel Continental de Marruecos":
+
+                                break;
+
+                            case "Hotel Continental de Osaka Tokio":
+                                break;
+
+                            default:
+                                Console.WriteLine("El hotel seleccionado no existe");
+                                break;
+                        }
+
+                        break;
+                    case "5":
+                        Console.WriteLine("Opcion 5");
+                        break;
+                    case "6":
+                        Console.WriteLine("Opcion 6");
+                        break;
+                    case "7":
+                        Console.WriteLine("Opcion 7");
+                        break;
+                    case "8":
+                        Console.WriteLine("Opcion 8");
+                        break;
+                    case "9":
+                        Console.WriteLine("Hasta luego");
+                        return;
+                    default:
+                        Console.WriteLine("Incorrecto");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private void subMenuOpcion2()
+        {
+            while (true)
+            {
+                Console.WriteLine("Submenú #1 \n" +
+                    "1- Listar Hotel Continental de New York\n" +
+                    "2- Listar Hotel Continental de Roma\n" +
+                    "3- Listar Hotel Continental de Marruecos\n" +
+                    "4- Listar Hotel Continental de Osaka Tokyo\n" +
+                    "5- Listar todos los hoteles\n" +
+                    "6- Salir");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                       
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "4":
+                        
+                        break;
+                    case "5":
+                        
+                        break;
+
+                    case "6":
+                        return;
+                    default:
+                        Console.WriteLine("Opcion incorrecta");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Practica-2-PrograAvanzada/Practica-2-PrograAvanzada/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2_PrograAvanzada
+{
+    internal class Persona
+    {
+        public String Nombre {  get; set; }
+        public string PrimerApellido {  get; set; }
+        public string SegundoApellido { get; set; }
+        public string Cedula {  get; set; }
+        public string Nacionalidad {  get; set; }
+        public string Telefono {  get; set; }
+        public string Correo {  get; set; }
+        public string Habitacion {  get; set; }
+
+        public Persona(string nombre, string primerApellido, string segundoApellido, string cedula, string nacionalidad, string telefono, string correo, string habitacion)
+        {
+            this.Nombre = nombre;
+            this.PrimerApellido = primerApellido;
+            this.SegundoApellido = segundoApellido;
+            this.Cedula = cedula;
+            this.Nacionalidad = nacionalidad;
+            this.Telefono = telefono;
+            this.Correo = correo;
+            this.Habitacion = habitacion;
+        }
+    }
+}
